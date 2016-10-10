3032f97395eec2d81a9d88a4350939089b065c9a
Eric W. Greene <eric@ericwgreene.com>
2016-10-10 14:41:41 -0400

    added code demos

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -4,16 +4,40 @@ import { Component } from '@angular/core';
 	selector: 'my-app',
 	template: `<h1>{{header}}</h1>
 <ul>
-	<li *ngFor="let color of colors">{{color | uppercase}}</li>
-</ul>`
+	<li *ngFor="let color of sortedColors;">{{color}}</li>
+</ul>
+<div>
+	<label for="new-color">New Color</label>
+	<input type="text" id="new-color" name="newColor" [(ngModel)]="newColor">
+	<button type="button" (click)="addColor(newColor)">Add Color</button>
+</div>`
 })
 export class AppComponent {
 
 	header: string = 'My Colorful App!';
+	newColor: string = '';
 
 	colors: string[] = [
 		'red','blue','black','apricot',
 		'gold','green','white','saffron'
 	];
+
+	lastColors: string[] = [];
+	_sortedColors: string[] = [];
+
+	addColor(nc: string) {
+		this.colors = this.colors.concat(nc);
+		this.newColor = '';
+	}
+
+	get sortedColors() : string[] {
+
+		if (this.lastColors != this.colors) {
+			console.log('sorting colors');
+			this._sortedColors = this.colors.concat().sort();
+			this.lastColors = this.colors;
+		}
+		return this._sortedColors;
+	}
 
 }
