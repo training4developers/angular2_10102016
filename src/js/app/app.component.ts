a6f6b7f1f7c4e3c9f78f657e90fad891cf28d839
Eric W. Greene <eric@ericwgreene.com>
2016-10-11 14:45:17 -0400

    added form code demos

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -1,41 +1,15 @@
 import { Component } from '@angular/core';
-import { Colors } from './services/colors';
 
 import '../../css/styles.scss';
 
 @Component({
 	selector: 'my-app',
 	styles: [require('./app.component.scss')],
-	template: require('./app.component.html'),
-	providers: [ Colors ]
+	template: require('./app.component.html')
 })
 export class AppComponent {
 
-	constructor(private colors: Colors) { }
+	firstName: string = '';
 
-	header: string = 'My Colorful App!';
-	newColor: string = '';
-	showMe: boolean = false;
-	colorFilter: string = '';
-	favColor: string = '';
-
-	addColor() {
-		this.colors.insert(this.newColor);
-		this.newColor = '';
-		this.colorFilterMap.clear();
-	}
-
-	colorFilterMap: Map<string, string[]> = new Map<string, string[]>();
-
-	get sortedColors() : string[] {
-
-		if (!this.colorFilterMap.has(this.colorFilter)) {
-			console.log('applying color filter');
-			this.colorFilterMap.set(this.colorFilter,
-				this.colors.getAll().filter(color => color.startsWith(this.colorFilter)));
-		}
-
-		return this.colorFilterMap.get(this.colorFilter);
-	}
 
 }
